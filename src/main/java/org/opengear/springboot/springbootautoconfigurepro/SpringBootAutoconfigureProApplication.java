83554ad37cd0b714a01f7f56d4405e7178bdc893
weian404 <weian404@gmail.com>
2024-01-04 21:29:26 +0800

    init

---
+++ src/main/java/org/opengear/springboot/springbootautoconfigurepro/SpringBootAutoconfigureProApplication.java
@@ -0,0 +1,13 @@
+package org.opengear.springboot.springbootautoconfigurepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoconfigureProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAutoconfigureProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/opengear/springboot/springbootautoconfigurepro/SpringBootAutoconfigureProApplicationTests.java
@@ -0,0 +1,13 @@
+package org.opengear.springboot.springbootautoconfigurepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoconfigureProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
